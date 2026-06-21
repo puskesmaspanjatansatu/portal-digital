27d0a681bc2131c9b5d4b20e729096fd2150680e
puskesmaspanjatansatu <puskesmaspanjatansatu@gmail.com>
2026-06-21 20:35:30 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,7 @@
-const CACHE_NAME = "portal-v1";
-
-self.addEventListener("install", () => {
+self.addEventListener("install",()=>{
 self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
+self.addEventListener("activate",()=>{
 self.clients.claim();
-});
-
-self.addEventListener("fetch", (event) => {
-event.respondWith(
-fetch(event.request).catch(() => caches.match(event.request))
-);
 });
